2e16c322a69a94df110b65233e72aed1e9a5741b
Aleksey Midenkov <midenok@gmail.com>
2020-12-11 20:14:42 +0300

    MDEV-21138 Assertion `col->ord_part' or `f.col->ord_part' failed in row_build_index_entry_low
    First part (row0mysql.cc) fixes ins_node_set_new_row() usage workflow
    as it is designed to operate on empty row (see row_get_prebuilt_insert_row()
    for example).
    
    Second part (row0ins.cc) fixes duplicate key error in FTS_DOC_ID_INDEX
    since history rows must not generate entries in that index. We detect
    FTS_DOC_ID_INDEX by a number of attributes and skip it if the row is
    historical.
    
    Misc fixes:
    
    row_build_index_entry_low() does not accept non-NULL tuple
    for FTS index (subject assertion fails), assertion (index->type !=
    DICT_FTS) adds code understanding.
    
    Now as historical_row is copied in row_update_vers_insert() there is
    no need to copy the row twice: ROW_COPY_POINTERS is used to build
    historical_row initially.

--- storage/innobase/row/row0ins.cc
+++ storage/innobase/row/row0ins.cc
@@ -3604,8 +3604,26 @@ row_ins(
 	ut_ad(node->state == INS_NODE_INSERT_ENTRIES);
 
 	while (node->index != NULL) {
-		if (node->index->type != DICT_FTS) {
+		dict_index_t *index = node->index;
+		/*
+		   We do not insert history rows into FTS_DOC_ID_INDEX because
+		   it is unique by FTS_DOC_ID only and we do not want to add
+		   row_end to unique key. Fulltext field works the way new
+		   DOC_ID is created on every fulltext UPDATE, so holding only
+		   DOC_ID for history is enough.
+		*/
+		if (index->type != DICT_FTS && (
+			!node->vers_history_row()
+			/* Assume n_uniq == 1 is only FTS_DOC_ID_INDEX */
+			|| !dict_index_is_unique(index)
+			|| dict_index_get_n_unique(index) > 1)) {
 			dberr_t err = row_ins_index_entry_step(node, thr);
+
+			ut_ad(!node->vers_history_row()
+			      || strcmp(index->name, FTS_DOC_ID_INDEX_NAME)
+			      || (dict_index_is_unique(index)
+				  && dict_index_get_n_unique(index) == 1));
+
 
 			if (err != DB_SUCCESS) {
 				DBUG_RETURN(err);
@@ -3765,4 +3783,12 @@ error_handling:
 	}
 
 	return(thr);
+}
+
+bool ins_node_t::vers_history_row() const
+{
+	if (!table->versioned())
+		return false;
+	dfield_t* row_end = dtuple_get_nth_field(row, table->vers_end);
+	return row_end->vers_history_row();
 }
